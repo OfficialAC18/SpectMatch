06a7b28ccda3130b392560bc34bc75d8dbc9bce0
OfficialAC18 <49880635+OfficialAC18@users.noreply.github.com>
2022-12-03 13:13:43 +0400

    Delete onnx_quant.py

--- onnx_quant.py
+++
@@ -1,6 +0,0 @@
-import onnx
-from onnxruntime.quantization import quantize_dynamic, QuantType
-
-model_fp32 = '/media/tower/d18329b1-fe35-43f8-8113-e096a40e5d3b/home/tower/Akchunya-Research/FixMatch (remasc) - PyTorch/model.onnx'
-model_quant = '/media/tower/d18329b1-fe35-43f8-8113-e096a40e5d3b/home/tower/Akchunya-Research/FixMatch (remasc) - PyTorch/model_quant_2.5s.onnx'
-quantized_model = quantize_dynamic(model_fp32, model_quant,weight_type=QuantType.QUInt8)
